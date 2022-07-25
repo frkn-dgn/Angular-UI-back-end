cc11fd9d24e721c370acd0c6aa03be9905c14511
frkn-dgn <furkan.dogan4238@gmail.com>
2022-07-25 23:52:04 +0300

    Asp.net

---
+++ Data/CardsDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Mission.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mission.Data
+{
+    public class CardsDbContext: DbContext
+    {
+
+        public CardsDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        //DbSet
+        public DbSet<Card> Cards { get; set; }
+    }
+}
